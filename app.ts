8c0edd42f262fbcfb3f1aadb517894bcc0c43779
Leonardo da Costa da Silva <leonardo.costa.silva@g.globo>
2023-08-04 15:08:08 -0300

    feat: Inicialização de servidor express

--- app.ts
+++ app.ts
@@ -1,1 +1,12 @@
-console.log("funciona")
+import express from "express";
+import { config } from "dotenv";
+
+config()
+
+const app = express()
+
+const port = process.env.PORT || 3000
+
+app.listen(port, ()=>{
+    console.log(`Servidor rodando em http://localhos:${port}`)
+})
